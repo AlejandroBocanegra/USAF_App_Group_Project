c749157e8891926a170c29fc064a7df1e91c10c7
AlejandroBocanegra <a.boca@outlook.com>
2020-07-10 07:01:57 +0900

    For Brandon
    its broken, here...fix it

--- server/database.sql
+++ server/database.sql
@@ -1,10 +1,3 @@
-psql -U usaf_user postgres;
-usafisthebest
-
-CREATE DATABASE usaf_app_group_project;
-
-\c usaf_app_group_project;
-
 CREATE TABLE amn (
     amn_id SERIAL PRIMARY KEY,
     amn_f_name varchar (15),
@@ -37,3 +30,90 @@ CREATE TABLE base (
     base_ntwrk_rites boolean,
     base_amn_id integer
 );
+
+--// Amn Sample Data //--
+
+INSERT INTO amn (amn_f_name, amn_m_i, amn_l_name, amn_rank, amn_sq, amn_flt, amn_office, amn_d_o_a, amn_d_licns, amn_o_s)
+VALUES ('Joe',
+        'C',
+        'Snuffy',
+        'SSgt',
+        '123 CS',
+        'ABCD',
+        'EFGH',
+        'Jun-04-2020',
+        'yes',
+        'no'), ('Jane',
+                'B',
+                'Smith',
+                'SrA',
+                '123 CS',
+                'ABCD',
+                'EFGH',
+                'Feb-02-2016',
+                'no',
+                'no'), ('John',
+                        'Q',
+                        'Public',
+                        'A1C',
+                        '123 CS',
+                        'ABCD',
+                        'EFGH',
+                        'Aug-24-2019',
+                        'yes',
+                        'no'), ('Maria',
+                                'A',
+                                'Salvador',
+                                'Amn',
+                                '123 CS',
+                                'ABCD',
+                                'EFGH',
+                                'Jul-04-2020',
+                                'yes',
+                                'no');
+
+--//sqd Sample Date //--
+
+INSERT INTO sqd (sqd_c_s_s, sqd_tempo_band, sqd_vulner_window, sqd_next_p_c_s, sqd_amn_id)
+VALUES ('no',
+        'blue',
+        'Jan-04-2021',
+        'Jun-05-2024',
+        1), ('yes',
+             'green',
+             'Aug-02-2020',
+             'Feb-03-2020',
+             2), ('yes',
+                  'yellow',
+                  'Feb-24-2020',
+                  'Aug-25-2023',
+                  3), ('yes',
+                       'blue',
+                       'Jan-04-2022',
+                       'Jul-05-2024',
+                       4);
+
+--//base Sample Date //--
+
+INSERT INTO base (base_m_p_f, base_new_come_brief, base_d_licns_brief, base_law_brief, base_ntwrk_rites, base_amn_id)
+VALUES ('yes',
+        'no',
+        'no',
+        null,
+        'yes',
+        1), ('yes',
+             'yes',
+             'no',
+             null,
+             'yes',
+             2), ('yes',
+                  'yes',
+                  'no',
+                  null,
+                  'yes',
+                  3), ('yes',
+                       'yes',
+                       null,
+                       null,
+                       'yes',
+                       4);
