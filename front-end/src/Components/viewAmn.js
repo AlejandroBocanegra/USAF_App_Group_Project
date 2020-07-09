60d6cc3e0c363d398f1362bd897a2947c0f2b8c0
AlejandroBocanegra <a.boca@outlook.com>
2020-07-10 03:18:53 +0900

    Edit before docker

--- front-end/src/Components/viewAmn.js
+++ front-end/src/Components/viewAmn.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 //Material UI
 const useStyles = makeStyles((theme) => ({
@@ -20,6 +21,24 @@ const useStyles = makeStyles((theme) => ({
         },
       },
 }));
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+//   const StyledTableRow = withStyles((theme) => ({
+//     root: {
+//       '&:nth-of-type(odd)': {
+//         backgroundColor: theme.palette.action.hover,
+//       },
+//     },
+//   }))(TableRow);
 //End Material UI
 
 const ViewAmn = () => {
@@ -49,78 +68,80 @@ const ViewAmn = () => {
 
     return (
         <Fragment>
+        <td>
             <TableContainer component={Paper}>
                 <Table className={classes.table} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell> Airman ID </TableCell>
-                            <TableCell> First Name </TableCell>
-                            <TableCell> Middle Initial </TableCell>
-                            <TableCell> Last Name </TableCell>
-                            <TableCell> Rank </TableCell>
-                            <TableCell> Squadron </TableCell>
-                            <TableCell> Flight </TableCell>
-                            <TableCell> Office Symbol </TableCell>
-                            <TableCell> Date of Arrival (DOA) </TableCell>
-                            <TableCell> Has a Drivers License </TableCell>
-                            <TableCell> Is Stationed Overseas </TableCell>
-                            <TableCell> Squadron ID </TableCell>
-                            <TableCell> Has In-processed with CSS </TableCell>
-                            <TableCell> Assigned Tempo Band </TableCell>
-                            <TableCell> Next Vulnerability Window </TableCell>
-                            <TableCell> Date Eligable to PCS </TableCell>
-                            <TableCell> Squadron Airman ID </TableCell>
-                            <TableCell> Base ID </TableCell>
-                            <TableCell> Has In-processed with MPF </TableCell>
-                            <TableCell> Has Attended New Commers Brief </TableCell>
-                            <TableCell> Has Attended Diving Brief </TableCell>
-                            <TableCell> Has Attended Foreign County Law Brief </TableCell>
-                            <TableCell> Has Obtained Correct Network Rights </TableCell>
-                            <TableCell> Base Airman ID </TableCell>
-                            <TableCell> Edit Airman </TableCell>
-                            <TableCell> Airman Has PCSd/PCAd Away </TableCell>
+                            <StyledTableCell> Airman ID </StyledTableCell>
+                            <StyledTableCell> First Name </StyledTableCell>
+                            <StyledTableCell> Middle Initial </StyledTableCell>
+                            <StyledTableCell> Last Name </StyledTableCell>
+                            <StyledTableCell> Rank </StyledTableCell>
+                            <StyledTableCell> Squadron </StyledTableCell>
+                            <StyledTableCell> Flight </StyledTableCell>
+                            <StyledTableCell> Office Symbol </StyledTableCell>
+                            <StyledTableCell> Date of Arrival (DOA) </StyledTableCell>
+                            <StyledTableCell> Has a Drivers License </StyledTableCell>
+                            <StyledTableCell> Is Stationed Overseas </StyledTableCell>
+                            <StyledTableCell> Squadron ID </StyledTableCell>
+                            <StyledTableCell> Has In-processed with CSS </StyledTableCell>
+                            <StyledTableCell> Assigned Tempo Band </StyledTableCell>
+                            <StyledTableCell> Next Vulnerability Window </StyledTableCell>
+                            <StyledTableCell> Date Eligable to PCS </StyledTableCell>
+                            <StyledTableCell> Squadron Airman ID </StyledTableCell>
+                            <StyledTableCell> Base ID </StyledTableCell>
+                            <StyledTableCell> Has In-processed with MPF </StyledTableCell>
+                            <StyledTableCell> Has Attended New Commers Brief </StyledTableCell>
+                            <StyledTableCell> Has Attended Diving Brief </StyledTableCell>
+                            <StyledTableCell> Has Attended Foreign County Law Brief </StyledTableCell>
+                            <StyledTableCell> Has Obtained Correct Network Rights </StyledTableCell>
+                            <StyledTableCell> Base Airman ID </StyledTableCell>
+                            <StyledTableCell> Edit Airman </StyledTableCell>
+                            <StyledTableCell> Airman Has PCSd/PCAd Away </StyledTableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {amn.map (amn => (
                             <TableRow>
-                                <TableCell> {amn.amn_id} </TableCell>
-                                <TableCell> {amn.first_name} </TableCell>
-                                <TableCell> {amn.mi} </TableCell>
-                                <TableCell> {amn.last_name} </TableCell>
-                                <TableCell> {amn.rank} </TableCell>
-                                <TableCell> {amn.squadron} </TableCell>
-                                <TableCell> {amn.flight} </TableCell>
-                                <TableCell> {amn.office_symbol} </TableCell>
-                                <TableCell> {amn.date_of_arrival} </TableCell>
-                                <TableCell> {amn.drivers_license} </TableCell>
-                                <TableCell> {amn.overseas} </TableCell>
-                                <TableCell> {amn.squadron_ID} </TableCell>
-                                <TableCell> {amn.css} </TableCell>
-                                <TableCell> {amn.tempo_band} </TableCell>
-                                <TableCell> {amn.vulnerability_window} </TableCell>
-                                <TableCell> {amn.eligable_to_pcs} </TableCell>
-                                <TableCell> {amn.squadron_amn_id} </TableCell>
-                                <TableCell> {amn.base_id} </TableCell>
-                                <TableCell> {amn.mpf} </TableCell>
-                                <TableCell> {amn.new_commers_brief} </TableCell>
-                                <TableCell> {amn.diving_brief} </TableCell>
-                                <TableCell> {amn.foreign_county_law_brief} </TableCell>
-                                <TableCell> {amn.network_rights} </TableCell>
-                                <TableCell> {amn.base_amn_id} </TableCell>
-                                <TableCell> 
+                                <StyledTableCell> {amn.amn_id} </StyledTableCell>
+                                <StyledTableCell> {amn.first_name} </StyledTableCell>
+                                <StyledTableCell> {amn.mi} </StyledTableCell>
+                                <StyledTableCell> {amn.last_name} </StyledTableCell>
+                                <StyledTableCell> {amn.rank} </StyledTableCell>
+                                <StyledTableCell> {amn.squadron} </StyledTableCell>
+                                <StyledTableCell> {amn.flight} </StyledTableCell>
+                                <StyledTableCell> {amn.office_symbol} </StyledTableCell>
+                                <StyledTableCell> {amn.date_of_arrival} </StyledTableCell>
+                                <StyledTableCell> {amn.drivers_license} </StyledTableCell>
+                                <StyledTableCell> {amn.overseas} </StyledTableCell>
+                                <StyledTableCell> {amn.squadron_ID} </StyledTableCell>
+                                <StyledTableCell> {amn.css} </StyledTableCell>
+                                <StyledTableCell> {amn.tempo_band} </StyledTableCell>
+                                <StyledTableCell> {amn.vulnerability_window} </StyledTableCell>
+                                <StyledTableCell> {amn.eligable_to_pcs} </StyledTableCell>
+                                <StyledTableCell> {amn.squadron_amn_id} </StyledTableCell>
+                                <StyledTableCell> {amn.base_id} </StyledTableCell>
+                                <StyledTableCell> {amn.mpf} </StyledTableCell>
+                                <StyledTableCell> {amn.new_commers_brief} </StyledTableCell>
+                                <StyledTableCell> {amn.diving_brief} </StyledTableCell>
+                                <StyledTableCell> {amn.foreign_county_law_brief} </StyledTableCell>
+                                <StyledTableCell> {amn.network_rights} </StyledTableCell>
+                                <StyledTableCell> {amn.base_amn_id} </StyledTableCell>
+                                <StyledTableCell> 
                                     <Button variant="contained" color="primary">
                                     Edit </Button>
-                                </TableCell>
-                                <TableCell> 
+                                </StyledTableCell>
+                                <StyledTableCell> 
                                     <Button variant="contained" color="secondary">
                                     Delete </Button>
-                                </TableCell>
+                                </StyledTableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
+            </td>
         </Fragment>
     );
 };
